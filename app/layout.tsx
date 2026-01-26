539422502584e6cf3ec8ea2ec8f1ec8861980c3d
superunicorns <unicorndad8@gmail.com>
2026-01-26 14:04:25 -0300

    Start Navbar styles and add logo to Navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Nunito } from 'next/font/google'
+import Navbar from './components/navbar/Navbar';
 import './globals.css'
 
 export const metadata: Metadata = {
@@ -18,7 +19,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   )
 }
