26b5f1c91a68e312dd8c9895b5173b6372cc1d12
superunicorns <unicorndad8@gmail.com>
2026-01-27 10:42:00 -0300

    Start modals development

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Nunito } from 'next/font/google'
 import Navbar from './components/navbar/Navbar';
+import Modal from './components/modals/Modal';
 import './globals.css'
 
 export const metadata: Metadata = {
@@ -20,6 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <Modal isOpen title="Hello World!" />
         <Navbar />
         {children}
       </body>
