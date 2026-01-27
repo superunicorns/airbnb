3ec1a9ea26f4988732e25ec1064e077f7c8513e4
superunicorns <unicorndad8@gmail.com>
2026-01-27 12:48:01 -0300

    Start RegisterModal component

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -4,8 +4,10 @@ import React, { useState, useCallback } from 'react';
 import { AiOutlineMenu } from 'react-icons/ai';
 import Avatar from '../Avatar';
 import MenuItem from './MenuItem';
+import useRegisterModal from '@/app/hooks/useRegisterModal';
 
 const UserMenu = () => {
+  const registerModal = useRegisterModal();
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleOpen = useCallback(() => {
@@ -15,7 +17,7 @@ const UserMenu = () => {
   return (
     <div className="relative">
       <div className="flex flex-row items-center gap-3">
-        <div onClick={() => {}} className="hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer">
+        <div onClick={() => {}} className="hidden px-4 py-3 text-sm font-semibold transition rounded-full cursor-pointer md:block hover:bg-neutral-100">
           Airbnb your home
         </div>
         <div onClick={toggleOpen} className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition">
@@ -29,7 +31,7 @@ const UserMenu = () => {
         <div className="flex flex-col cursor-pointer">
           <>
             <MenuItem onClick={() => {}} label="Login" />
-            <MenuItem onClick={() => {}} label="Sign Up" />
+            <MenuItem onClick={registerModal.onOpen} label="Sign Up" />
           </>
         </div>
       </div>)}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Nunito } from 'next/font/google'
 import Navbar from './components/navbar/Navbar';
-import Modal from './components/modals/Modal';
+import RegisterModal from './components/modals/RegisterModal';
 import './globals.css'
 
 export const metadata: Metadata = {
@@ -21,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Modal isOpen title="Hello World!" />
+        <RegisterModal />
         <Navbar />
         {children}
       </body>
