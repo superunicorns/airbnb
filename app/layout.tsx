8ff35c30c56c95c5f462abcb6210d1bbcc803805
superunicorns <unicorndad8@gmail.com>
2026-01-29 12:12:47 -0300

    Add Log In functionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,9 @@ import type { Metadata } from 'next'
 import { Nunito } from 'next/font/google'
 import Navbar from './components/navbar/Navbar';
 import RegisterModal from './components/modals/RegisterModal';
-import './globals.css'
 import ToasterProvider from './providers/ToasterProvider';
+import LoginModal from './components/modals/LoginModal';
+import './globals.css';
 
 export const metadata: Metadata = {
   title: 'Airbnb',
@@ -23,6 +24,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ToasterProvider />
+        <LoginModal />
         <RegisterModal />
         <Navbar />
         {children}
